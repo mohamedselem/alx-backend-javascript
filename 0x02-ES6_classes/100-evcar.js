d5597d9bdd2ff56228a614d230df539d63f7fc95
mohamedselem <the1ne_manga@yahoo.com>
2024-07-25 19:27:40 +0000

    fix

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,21 @@
 import Car from './10-car';
 
+// Define the EVCar class that extends the Car class
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    // Call the parent class constructor using the super keyword
     super(brand, motor, color);
-    this.range = range;
+    this._range = range; // Set the range property of the EVCar instance
   }
 
+  // Define a getter for the range property
   get range() {
     return this._range;
   }
 
-  set range(value) {
-    this._range = value;
-  }
-
+  // Define a method to clone the EVCar instance
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    // Create a new Car instance with the same brand, motor, and color
+    return new Car(this._brand, this._motor, this._color);
   }
 }
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,20 @@
-// Define the class name Building
 export default class Building {
-  // Constructor function that takes a parameter named sqft
   constructor(sqft) {
-    // Set the _sqft property to equal value of sqft
-    this._sqft = sqft;
-
-    // Check if current class is not the Building class 
-    // and if the evacuation Warning Message property is not defined in the current class
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      // Throw an error if the above condition is true
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
-  // Getter function for the sqft property
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
