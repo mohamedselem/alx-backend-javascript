11bc10806ed8d24875901e8369a1cb719d0e3e93
mohamedselem <the1ne_manga@yahoo.com>
2024-07-30 20:42:27 +0000

    all tasks

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author mohamed ali <https://github.com/mohamedselem>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,1 @@
+solving task 0x03-ES6_data_manipulation backend track
